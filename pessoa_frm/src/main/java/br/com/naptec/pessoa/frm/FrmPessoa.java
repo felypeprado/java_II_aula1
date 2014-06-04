3a6e80c641a96676d4e9831e589a8a341ad460d1
Felype Prado <xslaiderx@gmail.com>
2014-06-04 19:34:39 -0300

    Adicionado os métodos de pesquisa, alteração e exclusão.

--- pessoa_frm/src/main/java/br/com/naptec/pessoa/frm/FrmPessoa.java
+++ pessoa_frm/src/main/java/br/com/naptec/pessoa/frm/FrmPessoa.java
@@ -13,7 +13,9 @@ import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import br.com.naptec.base_dados.beans.Pessoa;
-import services.PessoaService;
+import br.com.naptec.pessoa.services.PessoaService;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class FrmPessoa extends JFrame {
 
@@ -22,8 +24,12 @@ public class FrmPessoa extends JFrame {
 	private JTextField txtNome;
 	private JTextField txtIdade;
 	private JTextField txtEmail;
-	
+	private final JButton btnLimpar = new JButton("Limpar");
+	private JButton btnGravar;
 	private PessoaService service;
+	private JButton btnEditar;
+	private JButton btnExcluir;
+	private Pessoa pessoa;
 
 	/**
 	 * Launch the application.
@@ -45,100 +51,217 @@ public class FrmPessoa extends JFrame {
 	 * Create the frame.
 	 */
 	public FrmPessoa() {
-		// Instaciar o objeto service da classe PessoaService.
+		//Instanciar o objeto service da classe PessoaService
 		service = new PessoaService();
+		// Instanciar o objeto pessoa da classe Pessoa
+		pessoa = new Pessoa();
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 228);
+		setBounds(100, 100, 530, 210);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblCodigo = new JLabel("C\u00F3digo");
-		lblCodigo.setBounds(10, 11, 40, 20);
-		contentPane.add(lblCodigo);
+		JLabel lblCdigo = new JLabel("C\u00F3digo");
+		lblCdigo.setBounds(10, 11, 55, 20);
+		contentPane.add(lblCdigo);
 		
 		txtCodigo = new JTextField();
-		txtCodigo.setBounds(60, 11, 86, 20);
+		txtCodigo.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyPressed(KeyEvent e) {
+				if (e.getKeyCode() == 10) {
+					pesquisar();
+				}
+			}
+		});
+		txtCodigo.setBounds(75, 11, 55, 20);
 		contentPane.add(txtCodigo);
 		txtCodigo.setColumns(10);
 		
 		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(10, 42, 40, 20);
+		lblNome.setBounds(10, 42, 55, 20);
 		contentPane.add(lblNome);
 		
 		txtNome = new JTextField();
-		txtNome.setBounds(60, 42, 364, 20);
+		txtNome.setBounds(75, 42, 429, 20);
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
 		
 		JLabel lblIdade = new JLabel("Idade");
-		lblIdade.setBounds(10, 73, 40, 20);
+		lblIdade.setBounds(10, 73, 55, 20);
 		contentPane.add(lblIdade);
 		
 		txtIdade = new JTextField();
-		txtIdade.setBounds(60, 73, 86, 20);
+		txtIdade.setBounds(75, 73, 55, 20);
 		contentPane.add(txtIdade);
 		txtIdade.setColumns(10);
 		
-		JLabel lblEmail = new JLabel("e-mail");
-		lblEmail.setBounds(10, 104, 40, 20);
+		JLabel lblEmail = new JLabel("Email");
+		lblEmail.setBounds(10, 104, 55, 20);
 		contentPane.add(lblEmail);
 		
 		txtEmail = new JTextField();
-		txtEmail.setBounds(60, 104, 364, 20);
+		txtEmail.setBounds(75, 104, 429, 20);
 		contentPane.add(txtEmail);
 		txtEmail.setColumns(10);
-		
-		JButton btnLimpar = new JButton("Limpar");
 		btnLimpar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				LimparCampos();
+				limparCampos();
 			}
 		});
-		btnLimpar.setBounds(10, 151, 89, 23);
+		btnLimpar.setBounds(10, 135, 90, 26);
 		contentPane.add(btnLimpar);
 		
-		JButton btnGravar = new JButton("Gravar");
+		btnGravar = new JButton("Gravar");
 		btnGravar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				gravar();
 			}
 		});
-		btnGravar.setBounds(109, 151, 89, 23);
+		btnGravar.setBounds(414, 135, 90, 26);
 		contentPane.add(btnGravar);
+		
+		JButton btnPesquisar = new JButton("Pesquisar");
+		btnPesquisar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				pesquisar();
+			}
+		});
+		btnPesquisar.setBounds(401, 10, 103, 23);
+		contentPane.add(btnPesquisar);
+		
+		btnEditar = new JButton("Editar");
+		btnEditar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				editar();
+			}
+		});
+		btnEditar.setBounds(113, 135, 89, 27);
+		contentPane.add(btnEditar);
+		
+		btnExcluir = new JButton("Excluir");
+		btnExcluir.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				excluir();
+			}
+		});
+		btnExcluir.setBounds(215, 135, 89, 26);
+		contentPane.add(btnExcluir);
 	}
 	
+	protected void excluir() {
+		if (JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(this, "Deseja excluir o registro?", "Confirmação", JOptionPane.YES_NO_OPTION)) {
+			try {
+				if (null != pessoa.getId() && pessoa.getId() > 0) {
+					service.excluir(pessoa.getId());
+				} else {
+					throw new Exception("Nenhum registro foi selecionado!");
+				}
+				
+				limparCampos();
+			} catch (Exception ex) {
+				JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+			}
+		}
+	}
+
+	protected void editar() {
+		txtNome.setEditable(true);
+		txtIdade.setEditable(true);
+		txtEmail.setEditable(true);
+	}
+
+	protected void pesquisar() {
+		try {
+			String sCod = txtCodigo.getText();
+			if ("".equals(sCod.trim())) {
+				throw new Exception("Campo código é obrigatório");
+			}
+			
+			Long cod = Long.parseLong(sCod);
+			pessoa = service.buscarPorId(cod);
+			
+			if (null == pessoa || pessoa.getId() == null) {
+				throw new Exception("Nenhum registro encontrado!");
+			}
+			
+			// Definindo os valores dos campos
+			txtCodigo.setText(pessoa.getId().toString());
+			txtNome.setText(pessoa.getNome().toString());
+			txtEmail.setText(pessoa.getEmail().toLowerCase());
+			txtIdade.setText(pessoa.getIdade() + "");			
+			
+			// Desabilitando os campos para edição
+			txtCodigo.setEditable(false);
+			txtNome.setEditable(false);
+			txtIdade.setEditable(false);
+			txtEmail.setEditable(false);
+		} catch (Exception ex) {
+			JOptionPane.showMessageDialog(this, ex.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
 	protected void gravar() {
 		try {
-			Pessoa p = new Pessoa();
+			pessoa = new Pessoa();
+			//O método getText() devolve uma String com o valor          
+			//do JTextField
+			if (!"".equals(txtNome.getText().trim())) {			
+				pessoa.setNome(txtNome.getText());
+			} else {
+				throw new Exception("O campo nome é obrigatório!");
+			}
+						
+			pessoa.setEmail(txtEmail.getText());
+			pessoa.setIdade(Integer.parseInt(txtIdade.getText()));
 			
-			// O método getText() devolve uma String com o valor do JTextField.
-			p.setNome(txtNome.getText());
-			p.setEmail(txtEmail.getText());
-			p.setIdade(Integer.parseInt(txtIdade.getText()));
+			String sId = txtCodigo.getText().trim();
+			if (!"".equals(sId)) {
+				pessoa.setId(Long.parseLong(sId));
+			}
+			service.gravar(pessoa);
 			
-			service.gravar(p);
+			JOptionPane.showMessageDialog(this, "Registro gravado com sucesso!", "Informação", JOptionPane.INFORMATION_MESSAGE);
+			
+			limparCampos();
 		} catch (Exception ex) {
-			JOptionPane.showMessageDialog(this, ex.getMessage());
+			JOptionPane.showMessageDialog(this, 
+					ex.getMessage());
 		}
-		
 	}
-
-	protected void LimparCampos() {
-		/*
-		 * setText(String): Define o texto do elemento JTextField;
-		 * 
-		 * */
+	
+	protected void limparCampos() {
+		//setText(String): Define o texto do elemento JTextField
 		this.txtCodigo.setText("");
-		this.txtNome.setText("");
-		this.txtIdade.setText("");
 		this.txtEmail.setText("");
+		this.txtIdade.setText("");
+		this.txtNome.setText("");
+		
+		// Habilitar os campos para edição
+		txtCodigo.setEditable(true);
+		txtNome.setEditable(true);
+		txtIdade.setEditable(true);
+		txtEmail.setEditable(true);
+		
+		pessoa = new Pessoa();
 	}
 
-	protected void Alerta() {
-		JOptionPane.showMessageDialog(this, "Funciona!");
-		
+	protected void alerta() {
+		//this = esta instância de objeto da classe FrmPessoa
+		JOptionPane.showMessageDialog(this, "Funciona!");		
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
